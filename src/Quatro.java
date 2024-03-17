243054997b0f667d19626379b9944d334d87b4a2
jurineide <jurineidesouza@gmail.com>
2024-03-17 16:27:07 -0300

    Quatro

--- src/Quatro.java
+++ src/Quatro.java
@@ -1,4 +1,27 @@
+import java.util.Scanner;
+
 public class Quatro {
     /*Escreva um programa que verifique se um número digitado pelo usuário é primo ou não.
      */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um numero");
+
+        int num = scanner.nextInt();
+        boolean numPrimo = true;
+
+        for (int i = 2; i <=Math.sqrt(num); i++){
+            if ( num % i ==0){
+                numPrimo = false;
+                break;
+            }
+        }
+        if(numPrimo && num > 1){
+            System.out.println(num + " é primo");
+        }else {
+            System.out.println(num + " não é primo");
+        }
+
+
+    }
 }
