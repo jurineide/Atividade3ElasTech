992660c035dfd9c542980ce6df56434e0d087b55
jurineide <jurineidesouza@gmail.com>
2024-03-17 15:54:41 -0300

    Um

--- src/Um.java
+++ src/Um.java
@@ -1,5 +1,12 @@
 public class Um {
     /*-Crie um programa que realize uma contagem regressiva de 10 até 1, mostrando cada número na tela.
      */
+    public static void main(String[] args) {
 
+
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+
+        }
+    }
 }
