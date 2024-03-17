eb48d9f1ae1d057106ab6acfca44c303782e0642
jurineide <jurineidesouza@gmail.com>
2024-03-17 18:40:13 -0300

    oito

--- src/Oito.java
+++ src/Oito.java
@@ -1,5 +1,33 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Oito {
     /* Implemente um jogo no qual o programa escolhe um número aleatório entre 1 e 100, e o jogador deve tentar adivinhá-lo.
      O programa deve fornecer dicas do tipo "maior" ou "menor" conforme necessário.
      */
+    public static void main(String[] args) {
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(" Tente adivinhar o numero!!");
+        int numAleatorio = random.nextInt(100) + 1;
+        int tentativa = 0;
+        int palpite;
+
+        do {
+            System.out.println("qual seu palpite: ");
+            palpite = scanner.nextInt();
+            tentativa++;
+
+            if (palpite < numAleatorio) {
+                System.out.println(" é maior!!!");
+            } else if (palpite > numAleatorio) {
+                System.out.println("é menor!!!!");
+            } else {
+                System.out.println("Acertou!!!!");
+            }
+
+        } while (palpite != numAleatorio);
+        scanner.close();
+    }
 }
