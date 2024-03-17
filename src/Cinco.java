81b4a10783c32233556a222c0ab5311b773cc9a8
jurineide <jurineidesouza@gmail.com>
2024-03-17 16:33:01 -0300

    Cinco

--- src/Cinco.java
+++ src/Cinco.java
@@ -1,4 +1,18 @@
 public class Cinco {
     /*Implemente um algoritmo que exiba os primeiros 20 números da sequência de Fibonacci.
      */
+    public static void main(String[] args) {
+        int num = 20;
+        int termo1 = 0;
+        int termo2 = 1;
+
+        for (int i = 1; i <= num; i++){
+            System.out.println(termo1 + " , ");
+
+            int termo3 = termo1 + termo2;
+            termo1 = termo2;
+            termo2 = termo3;
+
+        }
+    }
 }
