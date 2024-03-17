507f1c749ef4d7696cade7d72b59b2d98f462c6e
jurineide <jurineidesouza@gmail.com>
2024-03-17 16:09:09 -0300

    Dois

--- src/Tres.java
+++ src/Tres.java
@@ -1,4 +1,13 @@
 public class Tres {
     /* Desenvolva um algoritmo que calcule a soma dos números pares de 1 a 100
      */
+    public static void main(String[] args) {
+        int soma = 0;
+        for (int i = 1; i <= 100; i++){
+            if (i %2 == 0){
+                soma += i;
+            }
+        }
+        System.out.println(" A soma é: " + soma);
+    }
 }
