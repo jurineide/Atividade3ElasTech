89d7eac3a7eb974f12c9fbd2f668c802fdc019c0
jurineide <jurineidesouza@gmail.com>
2024-03-17 16:01:52 -0300

    Dois

--- src/Dois.java
+++ src/Dois.java
@@ -1,4 +1,18 @@
+import java.util.Scanner;
+
 public class Dois {
     /* Escreva um programa que solicite um número ao usuário e exiba a tabuada desse número de 1 a 10.
      */
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        System.out.println("Digite qual tabuada deseja ser exibida");
+        int tabuada = num.nextInt();
+
+        for(int i = 1; i <=10; i++){
+            int resposta = tabuada * i;
+            System.out.println(tabuada + " x "+ i + " = " + resposta);
+
+        }
+
+    }
 }
