0c7a6d5d3b2462294206928f0b86d9f294fb806b
jurineide <jurineidesouza@gmail.com>
2024-03-17 17:21:53 -0300

    Seis

--- src/Seis.java
+++ src/Seis.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Seis {
     /* Faça um programa que solicite ao usuário números inteiros positivos.
     Quando o usuário digitar um número negativo, o programa deve exibir a média dos números inseridos até aquele momento.
      */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int soma = 0;
+        int qtd = 0;
+
+        while (true) {
+            System.out.println("digite um numero: ");
+            int num = scanner.nextInt();
+
+            if (num >= 0) {
+                soma += num;
+                qtd++;
+            } else {
+                break;
+            }
+        }
+
+            double media =(double) soma/qtd;
+            System.out.println(" a média dos numeros digitados foram "+ media);
+    }
 }
